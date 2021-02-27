26e4c2c1fb47462199f438cb51c61d90a42be5e3
rodrigo <rodrigo_l321@hotmail.com>
2021-02-26 20:42:04 -0500

    Cambio de contraseña encriptada

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('julio','12345',1, 'Julio', 'Lopez','alumeno@bolsadeideas.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','12345',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('julio','$2a$10$U/2sWLHfgAyJajb7Kc8kIuq5f/m4aKc/5NGMYbBDXyHGOy/V6an6y',1, 'Julio', 'Lopez','alumeno@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$bVN34XOcNVC3v2HPLxk0feCokfUtotHzzCowcu8bNzBXbr75SaoCW',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
